c944029cff164f499b5ded1642760156f8682a35
klennox <klennox@tvo.org>
2019-10-28 17:03:50 -0400

    Half way thru section 23, video 245 - Building Our Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+/*
+Keith's notes
+
+- Created new react project.
+- Removed everything from the default project we don't need.
+- In App.js, I replaced the default functional component with a class component. React used to default to a class component, but no longer. Andrei uses class however.
+- Installed npm module called tachyons. Used "npm install tachyons" syntax. This handles CSS styling. Added accompanying inline styles using class names (no style sheet required).
+- Installed react-tilt. Used "npm install --save react-tilt" syntax. The package makes logos tilt when you roll over.
+- Added 4 components to the parent App component. Each component gets its own folder in a parent folder called src/Components.
+*/
+
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/*<ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
