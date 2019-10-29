2f3764e0c4dfab6de7b2dce56d0dce8732fd87e7
klennox <klennox@tvo.org>
2019-10-29 18:28:11 -0400

    Middle of section 23, video 248 - Image Recognition API

--- src/App.js
+++ src/App.js
@@ -17,20 +17,32 @@ WHAT I DID:
 - Added 5 new functional components to the parent App component.
   Gave each functional component its own folder and js file.
   Gave each functional component only basic presentational config. No real functionality.
+- Added code to handle the form input field and Detect button. As part of this, we have to use the Clarifai API (face recognition).
+  To make this work, I created an account at https://portal.clarifai.com/apps (user=klennox1@gmail.com + pw=unsecure);
+  added a couple of code snippets from the Carifai web site to App.js; in the terminal typed "npm install clarifai";
+  and added an import for Carifai to the top of App.js.
 
 QUESTIONS:
 - Not sure why Andaei likes to put JSX text nodes in curly braces. It still works without.
 - Why does Andaei sometimes use separate css style sheet and sometimes use tachyons?
 - My input field is much wider than Andrei's. No idea why.
+- Inide the class component, I don't understand the use of constructor() and super().
+  }
 */
 
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
 import './App.css';
+
+const app = new Clarifai.App({ //Start of code snippet#1 from Clarifai
+  apiKey: '52215e2d0e7d4c5fb5e0cb1486a66add'
+ }); //End of code snippet#1 from Clarifai
 
 const particlesOptions = {
   particles: {
@@ -45,6 +57,32 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() { //I don't know what this is.
+    super(); //I don't know what this is.
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict( //Start of code snippet#1 from Clarifai
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );//End of code snippet#1 from my Clarifai
+  }
+
   render() {
     return (
       <div className="App">
@@ -54,8 +92,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        /> {/* If we don't use () after onInputChange, the function is passed as a prop, but is not called. */}
+        <FaceRecognition />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => { //Normally you would put props here, we are destructuring props.
   return (
     <div>
       <p>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} /> {/* We don't have to use "props." before onInputChange because we destructured props above.*/}
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
